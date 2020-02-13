83658a6337f88c121e60f962f495ec661d05328d
zzrnature <zzr_nature@163.com>
2020-02-13 13:27:22 +0800

    Delete Bmob constant

--- app/src/main/java/com/example/woundmanage/Constants.java
+++ app/src/main/java/com/example/woundmanage/Constants.java
@@ -3,7 +3,7 @@ package com.example.woundmanage;
 public class Constants {
 
 
-    public static String Bmob_APPID = "67893c5b8d8829a456b0fd2153a19d59";
+//    public static String Bmob_APPID = "67893c5b8d8829a456b0fd2153a19d59";
 
     //public static final String BACK_PATIENT = "back_patient";
 
