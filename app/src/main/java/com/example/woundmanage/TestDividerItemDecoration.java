3440f2a590e46820ed5f10aa66c401b18db3c983
zzrnature <zzr_nature@163.com>
2020-02-18 22:36:52 +0800

    scroll view done; record editing page continue...

--- app/src/main/java/com/example/woundmanage/TestDividerItemDecoration.java
+++ app/src/main/java/com/example/woundmanage/TestDividerItemDecoration.java
@@ -16,22 +16,25 @@ public class TestDividerItemDecoration extends RecyclerView.ItemDecoration {
     public TestDividerItemDecoration() {
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        mPaint.setColor(Color.parseColor("#666666"));
+        //mPaint.setColor(Color.parseColor("#666666"));
     }
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
 
-//        //第一个ItemView不需要在上面绘制分割线
+        //第一个ItemView不需要在上面绘制分割线
         if (parent.getChildAdapterPosition(view) != 0){
             //这里直接硬编码为1px
             outRect.top = 1;
             mDividerHeight = 2;
         }
 
-        //outRect.right = 4;
-        //outRect.left = 80;
+//        outRect.top = 5;
+//
+//        outRect.bottom =5;
+//        outRect.left = 5;
+//        outRect.right = 5;
 
 
     }
