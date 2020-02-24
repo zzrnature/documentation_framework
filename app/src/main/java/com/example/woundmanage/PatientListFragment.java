e7c4a8d2293fb858c95051d8e466f71b777940c3
zzrnature <zzr_nature@163.com>
2020-02-24 14:00:37 +0800

    test over

--- app/src/main/java/com/example/woundmanage/PatientListFragment.java
+++ app/src/main/java/com/example/woundmanage/PatientListFragment.java
@@ -37,7 +37,6 @@ public class PatientListFragment extends Fragment {
     //private static final int REQUEST_CODE_PATIENT = 0;
 
 
-
     // Some initialization other than inflating the view
     // we do for the Bmob and menu here.
     @Override
