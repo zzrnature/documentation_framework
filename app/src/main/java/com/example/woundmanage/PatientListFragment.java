4bdcf383dfc7c885d03ca2f50d4e2873de78c67e
zzrnature <zzr_nature@163.com>
2020-02-24 13:56:40 +0800

    what the fuck 5

--- app/src/main/java/com/example/woundmanage/PatientListFragment.java
+++ app/src/main/java/com/example/woundmanage/PatientListFragment.java
@@ -36,6 +36,7 @@ public class PatientListFragment extends Fragment {
     // we use the start activity for result, so a code is needed to check with the result
     //private static final int REQUEST_CODE_PATIENT = 0;
 
+
     // Some initialization other than inflating the view
     // we do for the Bmob and menu here.
     @Override
