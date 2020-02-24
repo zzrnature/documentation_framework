1aab566bc878f45a8a93876203e245481114f580
zzrnature <zzr_nature@163.com>
2020-02-24 13:21:19 +0800

    little change 2

--- app/src/main/java/com/example/woundmanage/PatientListFragment.java
+++ app/src/main/java/com/example/woundmanage/PatientListFragment.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import androidx.recyclerview.widget.DividerItemDecoration;
